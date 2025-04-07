89b15645f6f34249ffbaf7f2797fee755c6eda9c
Jorge <ibarrapaezj@gmail.com>
2025-04-07 16:26:37 -0500

    se elimino el workflows de github

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://jorgedibarra.github.io',
-  base: 'jorgedibarra.github.io',
+  integrations: [tailwind()]
 });
