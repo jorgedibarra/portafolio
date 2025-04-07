7acb948f4c9017f2eedb9a4e464c069010529d72
Jorge <ibarrapaezj@gmail.com>
2025-04-07 16:53:32 -0500

    Configuracion de imageCDN

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
+  adapter: netlify({ imageCDN: false }),
   integrations: [tailwind()],
 });
