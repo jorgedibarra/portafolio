911810297e5f5ee9264088f39284d7baa6d69f6a
Jorge Ibarra <ibarrapaezj@gmail.com>
2026-04-22 16:08:18 -0500

    Apply remaining portfolio updates

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="astro/astro-jsx" />
+
+declare global {
+	namespace JSX {
+		interface IntrinsicElements extends astroHTML.JSX.IntrinsicElements {}
+	}
+}
